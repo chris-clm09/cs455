46e14bcb2fb375e62cc6325378d2aea88ad78962
Christopher Morgan <chris.clm09@gmail.com>
2013-01-11 17:42:37 -0800

    Inital project 2.

---
+++ project2.cpp
@@ -0,0 +1,213 @@
+#include <math.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <GL/glut.h>   // The GL Utility Toolkit (Glut) Header
+#include <iostream>
+using namespace std;
+
+/**********************************************************
+* // GLOBAL CRAP
+**********************************************************/
+const int SCREEN_HEIGHT = 640;
+const int SCREEN_WIDTH  = 480;
+const int RASTER_SIZE = SCREEN_HEIGHT * SCREEN_WIDTH * 3;
+float raster[RASTER_SIZE];
+int drawMode = 0;
+
+
+/**********************************************************
+Sets the clear color.
+**********************************************************/
+void clm_glClearColor(float r, float g, float b, float a)
+{
+   return;
+}
+
+/**********************************************************
+Clears the entire screen to the clear color.
+**********************************************************/
+void clm_glClear(GLint bit)
+{
+   return;
+}
+
+/**********************************************************
+This tells you how to interpret points, as will be 
+explained more below.
+// with parameters GL_POINTS, GL_LINES, and GL_TRIANGLES
+**********************************************************/
+void clm_glBegin(GLenum eVar)
+{
+   return;
+}
+
+/**********************************************************
+glVertex specifies a point for drawing, though how it is 
+drawn depends on the mode specified by glBegin. 
+glVertex2i(x,y) specifies the 4-vector point (x,y,0,1).
+//, plus possibly other glVertex calls
+**********************************************************/
+void clm_glVertex2i(int x, int y)
+{
+   return;
+}
+
+/**********************************************************
+There must be one glEnd for every glBegin. glVertex only 
+works between Begin & End pairs.
+**********************************************************/
+void clm_glEnd()
+{
+   return;
+}
+
+/**********************************************************
+There is one current color (a 4-vector) at all times in 
+OpenGL. Initially, it is (1,1,1,1) (white). Calling 
+glColor3f(r,g,b) sets it to (r,g,b,1). 
+**********************************************************/
+void clm_glColor3f(float r, float g, float b)
+{
+   return;
+}
+
+
+
+
+
+
+
+/**********************************************************
+* Initialize the raster. //?Don't know what a raster is.
+**********************************************************/
+void initRaster()
+{
+   float fRasterSize = ((float) RASTER_SIZE); 
+   for (int i = 0; i < RASTER_SIZE; i++)
+      raster[i] = i / fRasterSize;
+   return;
+}
+
+/**********************************************************
+*
+**********************************************************/
+void init ()
+{
+   initRaster();
+   glShadeModel(GL_SMOOTH);			// Enable Smooth Shading
+   glClearDepth(1.0f);					// Depth Buffer Setup
+   glEnable(GL_DEPTH_TEST);			// Enables Depth Testing
+   glDepthFunc(GL_LEQUAL);				// The Type Of Depth Testing To Do
+   glEnable(GL_COLOR_MATERIAL);
+   glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+}
+
+/**********************************************************
+*
+**********************************************************/
+void display ( void )   // Create The Display Function
+{
+	 glClearColor(1,0,0,1);  // Set the clear color
+   // Clear the screen to the clear color (i.e. if the clear color
+   // is red, the screen turns red);
+   glClear(GL_COLOR_BUFFER_BIT);
+
+   if (drawMode == 2) {
+      // Save the old state so that you can set it back after you draw
+      GLint oldmatrixmode;
+      GLboolean depthWasEnabled = glIsEnabled(GL_DEPTH_TEST);
+      glDisable(GL_DEPTH_TEST);
+      glGetIntegerv(GL_MATRIX_MODE,&oldmatrixmode);
+      glMatrixMode(GL_MODELVIEW); glPushMatrix(); glLoadIdentity();
+      glMatrixMode(GL_PROJECTION); glPushMatrix(); glLoadIdentity();
+      
+      // Draw the array of pixels (This is where you draw the values
+      // you have stored in the array 'raster')
+      glRasterPos2f(-1,-1);
+      glDrawPixels(SCREEN_HEIGHT,SCREEN_WIDTH,GL_RGB,GL_FLOAT,raster);
+      
+      //Set the state back to what it was
+      glPopMatrix();
+      glMatrixMode(GL_MODELVIEW); glPopMatrix();
+      glMatrixMode(oldmatrixmode);
+      if (depthWasEnabled)
+        glEnable(GL_DEPTH_TEST);
+   }
+   glFlush();
+
+	//If you are using glut, you will need to uncomment the following call:
+	glutSwapBuffers();
+}
+
+/**********************************************************
+*
+**********************************************************/
+void reshape ( int w, int h )   // Create The Reshape Function (the viewport)
+{
+	glViewport( 0, 0, w, h );
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+/**********************************************************
+*
+**********************************************************/
+void keyboard ( unsigned char key, int x, int y )  // Create Keyboard Function
+{
+	switch ( key ) 
+	{
+		case 27:        // When Escape Is Pressed...
+			exit ( 0 );  // Exit The Program
+			break;
+		case 49:
+		   drawMode = 1;
+		   display();
+		   break;
+		case 50:
+		   drawMode = 2;
+		   display();
+		   break;
+		default:
+			break;
+	}
+}
+
+/**********************************************************
+*
+**********************************************************/
+void arrow_keys ( int a_keys, int x, int y )  // Create Special Function (required for arrow keys)
+{
+  switch ( a_keys ) {
+    case GLUT_KEY_UP:     // When Up Arrow Is Pressed...
+      drawMode = (drawMode+1)%5;
+      display();
+      break;
+    case GLUT_KEY_DOWN:               // When Down Arrow Is Pressed...
+      if ((drawMode=drawMode-1) < 0)drawMode=4;
+      display();
+      break;
+    default:
+      break;
+  }
+}
+
+/**********************************************************
+*
+**********************************************************/
+int main ( int argc, char** argv )   // Create Main Function
+{
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE); // Display Mode
+  glutInitWindowSize(640, 480); // This is the window size
+  glutCreateWindow("OpenGL Example Program"); // Window Title
+  init();
+  glutDisplayFunc(display);  // Matching Earlier Functions To Their Counterparts
+  glutReshapeFunc(reshape);
+  glutKeyboardFunc(keyboard);
+  glutSpecialFunc(arrow_keys);
+  glutMainLoop();  // Initialize The Main Loop
+  return 0;
+}
+
