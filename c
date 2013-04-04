0c893e5d488afb1776884d2e3c382b9bfa972c3f
Christopher Morgan <junk.not@me.com>
2013-04-03 18:06:17 -0700

    Added header to example file.

---
+++ c
@@ -0,0 +1,2 @@
+rm a.out
+g++ -Ofast -l GL -l glut -l GLU -I cml-1_0_2/ example.cpp
--- example.cpp
+++ example.cpp
@@ -1,16 +1,4 @@
-#include <math.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <GL/glut.h>   // The GL Utility Toolkit (Glut) Header
-#include <iostream>
-#include "cml/cml.h"
-#include <vector>
-
-typedef cml::matrix44d_c matrix4;
-typedef cml::vector4d    vector4;
-
-#include "point.h"
-using namespace std;
+#include "example.h"
 
 /**********************************************************
 * // GLOBAL CRAP
